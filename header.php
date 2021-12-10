aee1e1b37b26b0f77d3a4d7fb5d60e864e6f0b9b
stephane-mathieu <mathieu.stephane@laplateforme.io>
2021-12-10 13:28:04 +0100

    fix-navbar

--- header.php
+++ header.php
@@ -28,55 +28,53 @@ $result_article_tri = mysqli_fetch_all($sql_recup, MYSQLI_ASSOC);
             <li><a href="articles.php">Articles</a></li>
             <li>
                 <ul>
-                <p>Catégories</p>
-                <?php foreach ($result_cat as $cat) { ?>
-                    <a href="article.php?id<?= $cat['id'][0] ?>"><?= $cat['nom'] ?> </a>
-                        <!-- <li>
-                        <a href="article.php?id='<?php $cat['id'] ?>'"><?php echo $cat['nom']; ?></a> -->
+                    <p>Catégories</p>
+                    <?php foreach ($result_cat as $cat) { ?>
+                        <a href="articles.php?categorie=<?= $cat['nom'] ?>"><?= $cat['nom'] ?> </a>
                         <?php
                         ?>
                 </ul>
             </li>
-    <?php } ?>
-    </li>
+        <?php } ?>
+        </li>
 
-    <!-- Si l'user n'est pas connecté -->
-    <?php if (!isset($_SESSION)) { ?>
-        <li>
-            <a href="inscription.php">Inscription</a>
-        </li>
-        <li>
-            <a href="connexion.php">Connexion</a>
-        </li>
-    <?php } ?>
+        <!-- Si l'user n'est pas connecté -->
+        <?php if (!isset($_SESSION)) { ?>
+            <li>
+                <a href="inscription.php">Inscription</a>
+            </li>
+            <li>
+                <a href="connexion.php">Connexion</a>
+            </li>
+        <?php } ?>
 
 
-    <!-- Si l'user est connecté -->
-    <?php if (isset($_SESSION)) { ?>
-        <li>
-            <a href="profil.php">Profil</a>
-        </li>
-        <li>
-            <form action="deconnexion.php" method="POST">
-                <button name="deconnexion">Deconnexion</button>
-            </form>
-        </li>
-    <?php } ?>
+        <!-- Si l'user est connecté -->
+        <?php if (isset($_SESSION)) { ?>
+            <li>
+                <a href="profil.php">Profil</a>
+            </li>
+            <li>
+                <form action="deconnexion.php" method="POST">
+                    <button name="deconnexion">Deconnexion</button>
+                </form>
+            </li>
+        <?php } ?>
 
-    <!-- SI admin ou modo -->
-    <?php
-    if (@$resultUser[0]["id_droits"] == "1337" || @$resultUser[0]["id_droits"] == "42") {
-    ?>
-        <li class="nav-item">
-            <a href="creer-articles.php">Créer articles</a>
-        </li>
-    <?php }; ?>
-    <?php
-    if (@$resultUser[0]["id_droits"] == "1337" || $resultUser[0]["id_droits"] == "42") {
-    ?><li class="nav-item">
-            <a href="admin.php">admin</a>
-        </li>
-    <?php }; ?>
-    </ul>
+        <!-- SI admin ou modo -->
+        <?php
+        if (@$resultUser[0]["id_droits"] == "1337" || @$resultUser[0]["id_droits"] == "42") {
+        ?>
+            <li class="nav-item">
+                <a href="creer-articles.php">Créer articles</a>
+            </li>
+        <?php }; ?>
+        <?php
+        if (@$resultUser[0]["id_droits"] == "1337" || $resultUser[0]["id_droits"] == "42") {
+        ?><li class="nav-item">
+                <a href="admin.php">admin</a>
+            </li>
+        <?php }; ?>
+        </ul>
     </nav>
 </header>
