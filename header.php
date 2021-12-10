4cca43208bb437704aea215ae3b9ae1c5a76d16f
stephane-mathieu <mathieu.stephane@laplateforme.io>
2021-12-10 13:33:37 +0100

    new-navbar

---
+++ header.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+// Connexion BDD utilisateurs
+$bdd = mysqli_connect('localhost', 'root', '', 'blog');
+mysqli_set_charset($bdd, 'utf-8');
+
+
+@$loogin = $_SESSION['user'];
+
+$queryUser = mysqli_query($bdd, "SELECT * FROM `utilisateurs` WHERE login = '$loogin'");
+$resultUser = mysqli_fetch_all($queryUser, MYSQLI_ASSOC);
+
+// Categorie
+$sql = mysqli_query($bdd, "SELECT categories.* FROM categories ");
+$result_cat = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+$sql_recup = mysqli_query($bdd, "SELECT categories.*, articles.id_categorie FROM categories INNER JOIN articles WHERE categories.id = articles.id_categorie;");
+$result_article_tri = mysqli_fetch_all($sql_recup, MYSQLI_ASSOC);
+
+// @$get = $_GET['categorie'];
+
+
+?>
+<header class="menu">
+    <nav class="menuNav">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="articles.php">Articles</a></li>
+            <li>
+                <ul>
+                <li>Catégories</li>
+                <?php foreach ($result_cat as $cat) { ?>
+                    <li>
+                    <a href="articles.php?categorie=<?= $cat['nom'] ?>"><?= $cat['nom'] ?> </a>
+                            <?php
+                            ?>
+                    </li>   
+                    <?php } ?>
+                </ul>
+            </li>
+
+            <!-- Si l'user n'est pas connecté -->
+            <?php if (!isset($_SESSION)) { ?>
+                <li><a href="inscription.php">Inscription</a></li>
+                <li><a href="connexion.php">Connexion</a></li>
+            <?php } ?>
+
+
+            <!-- Si l'user est connecté -->
+            <?php if (isset($_SESSION)) { ?>
+                <li><a href="profil.php">Profil</a></li>
+                <li>
+                    <form action="deconnexion.php" method="POST">
+                        <button name="deconnexion">Deconnexion</button>
+                    </form>
+                </li>
+            <?php } ?>
+
+            <!-- Si modo ou admin-->
+            <?php if (@$resultUser[0]["id_droits"] == "1337" || @$resultUser[0]["id_droits"] == "42") { ?>
+                <li class="nav-item">
+                    <a href="creer-articles.php">Créer articles</a>
+                </li>
+            <?php }; ?>
+
+            <!-- Si admin -->
+            <?php if (@$resultUser[0]["id_droits"] == "1337" || $resultUser[0]["id_droits"] == "42") { ?>  
+                <li class="nav-item">
+                    <a href="admin.php">admin</a>
+                </li>
+            <?php }; ?>
+        </ul>
+    </nav>
+</header>
