82ca001bfd693da1ab624b6639ed24e735c10215
Boris Tikhomiroff <boris.tikhomiroff@laplateforme.io>
2021-12-14 11:55:11 +0100

    add footer boris branch

---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="articles.php">Articles</a></li>
+
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="connexion.php">connexion</a></li>
+
+            <li><a href="profil.php">Profil</a></li>
+            <li><button>Deconnexion</button></a></li>
+        </ul>
+    </nav>
+</footer>
